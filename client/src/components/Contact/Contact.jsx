4dacc647b7e73b49067e27673b4d3b2b590adc49
Juan Pablo Bustamante <cuchobustamante@gmail.com>
2022-03-22 08:48:10 -0500

    single portfolio upgraded

--- client/src/components/Contact/Contact.jsx
+++ client/src/components/Contact/Contact.jsx
@@ -2,25 +2,33 @@ import React from "react";
 
 export default function Contact() {
   return (
-    <section id="contact">
-      <h2> Contact Me </h2>
+    <div className="section-teal">
+      <section id="contact">
+        <h2> Contact Me </h2>
 
-      <p>
-        {" "}
-        I'm Always interested to new oportunities in software, finance,
-        logistics and supply chain. Please feel welcomed to reach me through
-        here or Linkedin anytime. I will do my best effort to respond as quickly
-        as possible
-      </p>
+        <p>
+          {" "}
+          I'm Always interested to new oportunities in tech, finance,
+          logistics and supply chain. Please feel welcomed to reach me through
+          here or Linkedin anytime. I will do my best effort to respond as
+          quickly as possible
+        </p>
 
-      <p>
-        <a href="mailto:cuchobustamante@gmail.com">Email me</a>
-      </p>
-      <p>
-        <a href="https://www.linkedin.com/in/jpbust/" target="_blank">
-          LinkedIn
-        </a>
-      </p>
-    </section>
+        <p>
+          <a
+            className="button"
+            href="mailto:cuchobustamante@gmail.com"
+            target="_blank"
+          >
+            Email me
+          </a>
+        </p>
+        <p>
+          <a href="https://www.linkedin.com/in/jpbust/" target="_blank">
+            LinkedIn
+          </a>
+        </p>
+      </section>
+    </div>
   );
 }
