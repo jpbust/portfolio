e40114939eae7b0733fbd210dfcd791193f31cdd
Juan Pablo Bustamante <cuchobustamante@gmail.com>
2022-05-01 08:35:34 -0500

    added browserhistory url search and custom routes

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,22 +1,35 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
+import { Routes, Route, browserHistory } from "react-router-dom";
 import Nav from "./Nav/Nav.jsx";
 import Intro from "./Intro/Intro.jsx";
 import Contact from "./Contact/Contact.jsx";
 import Footer from "./Footer/Footer.jsx";
 import GradientLine from "./GradientLine.jsx";
 import { Projects } from "./Projects/Projects.jsx";
+import { AboutMe } from "../routes/AboutMe.jsx";
+import { NotFound } from "../routes/NotFound.jsx";
+
+const landingPage = (
+  <>
+    <Intro />
+    <GradientLine />
+    <Projects />
+    <GradientLine />
+    <Contact />
+    <GradientLine />
+  </>
+);
 
 const App = () => {
   return (
     <div className="mainContainer">
       <Nav />
-      <GradientLine />
-      <Intro />
-      <GradientLine />
-      <Projects />
-      <GradientLine />
-      <Contact />
-      <GradientLine />
+      <Routes>
+        <Route path="/" element={landingPage}></Route>
+        <Route path="/aboutme" element={<AboutMe />} />
+
+        <Route path="/*" element={<NotFound />} />
+      </Routes>
       <Footer />
     </div>
   );
--- client/src/components/Nav/Nav.jsx
+++ client/src/components/Nav/Nav.jsx
@@ -1,58 +1,73 @@
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faEnvelope, faLaptopCode } from "@fortawesome/free-solid-svg-icons";
 import React from "react";
+import { Link } from "react-router-dom";
+import { HashLink } from "react-router-hash-link";
 import { faGithub, faLinkedin } from "@fortawesome/free-brands-svg-icons";
+import GradientLine from "../GradientLine";
 
 const Nav = () => {
   return (
-    <nav id="nav">
-      <ul>
-        <li>
-          <h1>
-            <a href="index.html" className="nameTop">
+    <div className="navBarContainer">
+      <nav id="nav">
+        <ul>
+          <li>
+            <h1>
+              <Link to="/">
+                <a className="nameTop">
+                  <FontAwesomeIcon
+                    className="fa-brands fa-laptop-code"
+                    icon={faLaptopCode}
+                  />
+                  <span>Juan Pablo Bustamante</span>
+                </a>
+              </Link>
+            </h1>
+          </li>
+          <li>
+            <HashLink to="/#projects">
+              <a>Projects</a>
+            </HashLink>
+          </li>
+          <li>
+            <Link to="aboutme">About</Link>
+          </li>
+          <li>
+            <HashLink to="/#contact">
+              <a>Contact</a>
+            </HashLink>
+          </li>
+          <li>
+            <a href="https://www.linkedin.com/in/jpbust/" target="_blank">
               <FontAwesomeIcon
-                className="fa-brands fa-laptop-code"
-                icon={faLaptopCode}
+                className="fa-brands fa-linkedin"
+                icon={faLinkedin}
               />
-              <span>Juan Pablo Bustamante</span>
+              <span className="sr-only">LinkedIn</span>
             </a>
-          </h1>
-        </li>
-        <li>
-          <a href="#projects">Projects</a>
-        </li>
-        <li>
-          <a href="about.html">About</a>
-        </li>
-        <li>
-          <a href="#contact">Contact</a>
-        </li>
-        <li>
-          <a href="https://www.linkedin.com/in/jpbust/" target="_blank">
-            <FontAwesomeIcon
-              className="fa-brands fa-linkedin"
-              icon={faLinkedin}
-            />
-            <span className="sr-only">LinkedIn</span>
-          </a>
-        </li>
-        <li>
-          <a href="https://github.com/jpbust" target="_blank">
-            <FontAwesomeIcon className="fa-brands fa-github" icon={faGithub} />
-            <span className="sr-only">Github</span>
-          </a>
-        </li>
-        <li>
-          <a
-            href="https://docs.google.com/document/d/1kh3QkzdQTP5Mq7s4-1j-SXaA5S4qPTnz/edit?usp=sharing&ouid=112101043673644164577&rtpof=true&sd=true"
-            target="_blank"
-            className="button"
-          >
-            Resume
-          </a>
-        </li>
-      </ul>
-    </nav>
+          </li>
+          <li>
+            <a href="https://github.com/jpbust" target="_blank">
+              <FontAwesomeIcon
+                className="fa-brands fa-github"
+                icon={faGithub}
+              />
+              <span className="sr-only">Github</span>
+            </a>
+          </li>
+          <li>
+            <a
+              href="https://docs.google.com/document/d/1kh3QkzdQTP5Mq7s4-1j-SXaA5S4qPTnz/edit?usp=sharing&ouid=112101043673644164577&rtpof=true&sd=true"
+              target="_blank"
+              className="button"
+            >
+              Resume
+            </a>
+          </li>
+        </ul>
+      </nav>
+      <GradientLine />
+    </div>
   );
 };
 
