a17f5869256a6ef2b11b0d7f10e631d57a392133
Juan Pablo Bustamante <cuchobustamante@gmail.com>
2022-05-02 14:18:25 -0500

    deployment v1 ready

--- client/src/routes/AboutMe.jsx
+++ client/src/routes/AboutMe.jsx
@@ -2,9 +2,35 @@ import React from "react";
 
 export const AboutMe = () => {
   return (
-    <div>
-      AboutMe
-      <div>hi</div>
+    <div className="aboutMeContainer">
+      <img
+        src="https://res.cloudinary.com/jpbust/image/upload/v1651495889/portfolio/profile_pic_h1zruv.jpg"
+        alt="juan pablo profile picture"
+        className="aboutMePicture"
+      ></img>
+      <div>
+        <p className="aboutMeIntro">
+          I'm Juan Pablo, a Full-Stack Software Developer working for{" "}
+          <a href="https://www.thryv.com">Thryv</a> as Front-End Web Developer
+          for the payments platform (
+          <a href="https://www.thryv.com/features/thryvpay/">Thryv pay</a>)
+          remotely in the USA.{" "}
+        </p>{" "}
+        <p>
+          I've spent my early professional career working the business side of
+          International logistics and Supply Chain and later decided to
+          transition into software because of my never-ending passion of
+          creating.
+        </p>
+        <p>
+          These days I spend my time researching new frameworks, improving my
+          Javascript knowledge and coding.
+        </p>
+        <p>
+          Outside of the office I'm a full-time dad of two girls and two dogs
+          and also, a Crossfit enthusiast among other passion I pursue.
+        </p>
+      </div>
     </div>
   );
 };
--- client/src/routes/NotFound.jsx
+++ client/src/routes/NotFound.jsx
@@ -3,6 +3,11 @@ import React from "react";
 export const NotFound = () => {
   return (
     <div className="pageNotFound">
+      <img
+        src="https://res.cloudinary.com/jpbust/image/upload/v1651494877/portfolio/tarzan404_uyfwbo.jpg"
+        alt="sad dog picture"
+        className="notFoundImage"
+      />
       <h1>404 - Page not found</h1>
     </div>
   );
